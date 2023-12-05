46c6b3fabf0d27bcbf3869d6828fe4504c00faa5
swatisaulanki <swatisaulanki@gmail.com>
2023-12-05 14:34:19 +0530

    first-commit

---
+++ src/Components/Counter/Counters.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Counters = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Counters
---
+++ src/Components/Fetch/Fetching.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Fetching = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Fetching
